013f430cc962d8934a60ec095fab8ab44a6c2925
Robin Spruytte <robin.spruytte646@gmail.com>
2019-04-03 22:23:20 +0200

    added hexmover.js with HexMover class, refactored for server

---
+++ js/game/world_objects/hexmover.js
@@ -0,0 +1,152 @@
+/*jshint esversion: 6 */
+
+class HexMover {
+  constructor(map, x, y) {
+    this.map = map;
+    this.currentTile = this.map.getTile({ x: x, y: y });
+    this.currentPosition = this.updateCurrentPosition();
+    this.currentRotation = 1;
+    this.weapons = {
+      // base weapon always equiped
+      weaponName: ["gatling gun"],
+      weaponDamage: [4],
+      weaponRange: [20]
+    };
+    this.addons = [null, null, null];
+    this.addonUses = [0, 0, 0];
+    this.health = 20;
+    this.canEnterWater = false;
+    this.tankMovementRange = 1;
+  }
+
+  //movement
+  forward() {
+    let newCubeLocation = {};
+
+    if (this.currentRotation == 1) {
+      //y ==, z-1,x+1
+      newCubeLocation = {
+        x: this.currentTile.cubePosition.x + 1,
+        y: this.currentTile.cubePosition.y,
+        z: this.currentTile.cubePosition.z - 1
+      };
+    } else if (this.currentRotation == 2) {
+      //y - 1, z ==; x + 1
+      newCubeLocation = {
+        x: this.currentTile.cubePosition.x + 1,
+        y: this.currentTile.cubePosition.y - 1,
+        z: this.currentTile.cubePosition.z
+      };
+    } else if (this.currentRotation == 3) {
+      //y - 1, z + 1, x ==
+      newCubeLocation = {
+        x: this.currentTile.cubePosition.x,
+        y: this.currentTile.cubePosition.y + 1,
+        z: this.currentTile.cubePosition.z + 1
+      };
+    } else if (this.currentRotation == 4) {
+      //y ==, z + 1, x - 1
+      newCubeLocation = {
+        x: this.currentTile.cubePosition.x - 1,
+        y: this.currentTile.cubePosition.y,
+        z: this.currentTile.cubePosition.z + 1
+      };
+    } else if (this.currentRotation == 5) {
+      //y +1, z ==, x -1
+      newCubeLocation = {
+        x: this.currentTile.cubePosition.x - 1,
+        y: this.currentTile.cubePosition.y + 1,
+        z: this.currentTile.cubePosition.z
+      };
+    } else if (this.currentRotation == 6) {
+      //y + 1, z - 1, x ==
+      newCubeLocation = {
+        x: this.currentTile.cubePosition.x,
+        y: this.currentTile.cubePosition.y + 1,
+        z: this.currentTile.cubePosition.z - 1
+      };
+    }
+    let newPosition = this.map.cubeToOddr(newCubeLocation);
+    this.setPosition(newPosition.x, newPosition.y);
+  }
+
+  backward() {
+    let newCubeLocation = {};
+
+    if (this.currentRotation == 1) {
+      //y ==, z-1,x+1
+      newCubeLocation = {
+        x: this.currentTile.cubePosition.x - 1,
+        y: this.currentTile.cubePosition.y,
+        z: this.currentTile.cubePosition.z + 1
+      };
+    } else if (this.currentRotation == 2) {
+      //y - 1, z ==; x + 1
+      newCubeLocation = {
+        x: this.currentTile.cubePosition.x - 1,
+        y: this.currentTile.cubePosition.y + 1,
+        z: this.currentTile.cubePosition.z
+      };
+    } else if (this.currentRotation == 3) {
+      //y - 1, z + 1, x ==
+      newCubeLocation = {
+        x: this.currentTile.cubePosition.x,
+        y: this.currentTile.cubePosition.y - 1,
+        z: this.currentTile.cubePosition.z - 1
+      };
+    } else if (this.currentRotation == 4) {
+      //y ==, z + 1, x - 1
+      newCubeLocation = {
+        x: this.currentTile.cubePosition.x + 1,
+        y: this.currentTile.cubePosition.y,
+        z: this.currentTile.cubePosition.z - 1
+      };
+    } else if (this.currentRotation == 5) {
+      //y +1, z ==, x -1
+      newCubeLocation = {
+        x: this.currentTile.cubePosition.x + 1,
+        y: this.currentTile.cubePosition.y - 1,
+        z: this.currentTile.cubePosition.z
+      };
+    } else if (this.currentRotation == 6) {
+      //y + 1, z - 1, x ==
+      newCubeLocation = {
+        x: this.currentTile.cubePosition.x,
+        y: this.currentTile.cubePosition.y - 1,
+        z: this.currentTile.cubePosition.z + 1
+      };
+    }
+    let newPosition = this.map.cubeToOddr(newCubeLocation);
+    this.setPosition(newPosition.x, newPosition.y);
+  }
+
+  left() {
+    if (this.currentRotation != 1) {
+      this.currentRotation = this.currentRotation - 1;
+    } else {
+      this.currentRotation = 6;
+    }
+  }
+
+  right() {
+    if (this.currentRotation != 6) {
+      this.currentRotation = this.currentRotation + 1;
+    } else {
+      this.currentRotation = 1;
+    }
+  }
+
+  setPosition(x, y) {
+    if (this.map.getTile({ x: x, y: y })) {
+      this.currentTile = this.map.getTile({ x: x, y: y });
+      this.currentPosition = this.updateCurrentPosition();
+    }
+  }
+
+  updateCurrentPosition() {
+    return this.map.cubeToOddr(this.currentTile.cubePosition);
+  }
+}
+
+
+module.exports = HexMover;
