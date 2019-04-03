a50fa212fecf621da906e85eb79fb9a478a75d5f
Robin Spruytte <robin.spruytte646@gmail.com>
2019-04-03 22:27:24 +0200

    added fsmanipulator.js with file system module function readFile

---
+++ js/io/fsmanipulator.js
@@ -0,0 +1,8 @@
+/*jshint esversion: 6 */
+
+const fs = require('fs'); //file system
+
+//expects string path, returns the content of the specified file, if found
+exports.readFile = (path) => {
+    return fs.readFileSync(path);
+};
