22616f2dc88780034ca0e3a9930d120f63bb14a1
Robin Spruytte <robin.spruytte646@gmail.com>
2019-04-03 22:29:25 +0200

    added userinputhandler.js file with initial UserInputHandler class

---
+++ js/io/userinputhandler.js
@@ -0,0 +1,10 @@
+/*jshint esversion: 6 */
+
+class UserInputHandler {
+    constructor (game, mqtt) {
+        this.game = game;
+        this.mqtt = mqtt;
+    }
+}
+
+module.exports = UserInputHandler;
