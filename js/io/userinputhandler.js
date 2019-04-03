8125e9b86055f5af4a530ae361c49b80289f54b4
Robin Spruytte <robin.spruytte646@gmail.com>
2019-04-03 22:30:46 +0200

    added onUserInput method to UserInputHandler class

--- js/io/userinputhandler.js
+++ js/io/userinputhandler.js
@@ -6,12 +6,31 @@ class UserInputHandler {
         this.mqtt = mqtt;
     }
 
-     //expects a json string, will a json object if the given string is a valid json or else null
-     parseJson (json) {
+    //expects a json string, will a json object if the given string is a valid json or else null
+    parseJson (json) {
         try {
             return JSON.parse(json);
         } catch (error) {
             return null;
+        }
+    }
+
+    //expects a string topic and a json string message, will determine what to update in the game state with the given input
+    //special case: if the given player name does not exist yet, will create this new player and will ignore this new players movement input for that instance
+    onUserInput (topic, message) {
+        let input = this.parseJson(message);
+        if (input) {
+            if (input.Player) {
+                //console.log(input);
+                let player = this.game.playerList.getPlayer(input.Player.username);
+                if (player) {
+                    this.handleInput(player, input);
+                } else {
+                    this.onNewPlayerConnected(input.Player.username);
+                }
+            }
+        } else if (topic == this.mqtt.topics.replicated) {
+            this.mqtt.log("the given input was invalid");
         }
     }
 }
