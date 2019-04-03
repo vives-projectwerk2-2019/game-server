ebf662a36d70fa014b59f4c9f0149c477e439dae
Robin Spruytte <robin.spruytte646@gmail.com>
2019-04-03 22:20:30 +0200

    added playerlist.js file with initial PlayerList class

---
+++ js/game/players/playerlist.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+
+const Player = require(__lib + '/game/players/player.js');
+
+class PlayerList {
+    constructor () {
+        this.players = [];
+    }
+}
+
+module.exports = PlayerList;
