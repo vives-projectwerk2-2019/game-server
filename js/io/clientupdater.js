0dc6aece727f83393532193bb8a21e3f9e1bb8fb
Robin Spruytte <robin.spruytte646@gmail.com>
2019-04-03 22:26:25 +0200

    added update method to ClientUpdater class

--- js/io/clientupdater.js
+++ js/io/clientupdater.js
@@ -4,6 +4,10 @@ class ClientUpdater {
     constructor (mqtt) {
         this.mqtt = mqtt;
     }
+
+    update(gameState){
+        this.mqtt.replicate(gameState);
+    }
 }
 
 module.exports = ClientUpdater;
