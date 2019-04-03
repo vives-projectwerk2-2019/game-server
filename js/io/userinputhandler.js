690c8fb89e8dcfd0118b9f1ecbb1ee58382f954c
Robin Spruytte <robin.spruytte646@gmail.com>
2019-04-03 22:30:09 +0200

    added parseJson method to UserInputHandler class

--- js/io/userinputhandler.js
+++ js/io/userinputhandler.js
@@ -5,6 +5,15 @@ class UserInputHandler {
         this.game = game;
         this.mqtt = mqtt;
     }
+
+     //expects a json string, will a json object if the given string is a valid json or else null
+     parseJson (json) {
+        try {
+            return JSON.parse(json);
+        } catch (error) {
+            return null;
+        }
+    }
 }
 
 module.exports = UserInputHandler;
