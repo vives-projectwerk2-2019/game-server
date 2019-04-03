4769763aced58de0a26ad6d6a0fd9079c5130fde
Robin Spruytte <robin.spruytte646@gmail.com>
2019-04-03 22:21:02 +0200

    added addPlayer method to PlayerList class

--- js/game/players/playerlist.js
+++ js/game/players/playerlist.js
@@ -6,6 +6,15 @@ class PlayerList {
     constructor () {
         this.players = [];
     }
+
+    //will add a new player to the end of the list with specified string name and Tank tank
+    addPlayer(name, tank){
+        if (!this.getPlayer(name)){
+            this.players.push(new Player(name, tank));
+        } else {
+            console.log("the specified name is already in use");
+        }
+    }
 }
 
 module.exports = PlayerList;
