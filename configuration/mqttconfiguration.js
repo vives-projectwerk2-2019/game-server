1a9d9bf5e05e99e0bc99fc8a184880fc6053335a
Sille Van Landschoot <info@sillevl.be>
2019-05-01 12:53:17 +0200

    enable websockets on mosqa broker

--- configuration/mqttconfiguration.js
+++ configuration/mqttconfiguration.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 
 module.exports = {
     "settings": {
-      "port": parseInt(process.env.PORT) || 1884
+      "port": parseInt(process.env.PORT) || 1884,
+      "http": {"port": 9001, "bundle": true, "static": './'}
     },
     "broker": process.env.BROKER || "mqtt://labict.be",
     "topics": {
