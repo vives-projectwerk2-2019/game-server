2299a8595cd42fd717f9c8a6130602b51340ed1b
Sille Van Landschoot <info@sillevl.be>
2019-05-02 14:15:26 +0200

    fix broker logging

--- js/connection/broker.js
+++ js/connection/broker.js
@@ -6,9 +6,15 @@ class Broker {
 	//expects json settings with port, a function to execute after the broker has been set up may also be provided
 	constructor(settings, onLoaded = null){
 		this.server = new mosca.Server(settings);
-		this.server.on("clientConnected", (client) => { console.log(`Client connected: ${client.id}`)});
-		this.server.on("clientDisconnected", (client) => { console.log(`Client disconnected: ${client.id}`)});
-		this.server.on("published", (packet, client) => { console.log(`Client (${client.id}) published packet`)});
+		this.server.on("clientConnected", function(client) {
+			console.log('Client Connected:', client.id);
+			});
+		this.server.on("clientDisconnected",function(client) {
+			console.log('Client Disconnected:', client.id);
+			});
+		this.server.on("published", function(packet, client) {
+			console.log('Client published message:', client.id);
+			});
 		this.server.on('ready', function(){
 			console.log("Broker ready");
 			onLoaded();
