e256cf448caa422ec214f67f10486f8b74052484
Yoshi Hollez <yoshi.hollez@student.vives.be>
2019-05-02 12:33:11 +0200

    changed mqtt broker of game

--- configuration/mqttconfiguration.js
+++ configuration/mqttconfiguration.js
@@ -5,7 +5,7 @@ module.exports = {
     port: parseInt(process.env.PORT) || 1884,
     http: { port: 9001, bundle: true, static: "./" }
   },
-  broker: process.env.BROKER || "mqtt://labict.be",
+  broker: process.env.BROKER || "wss://game.bug.labict.be/broker",
   apiBroker: "wss://api.bug.labict.be/broker",
   topics: {
     main: process.env.MAIN_TOPIC || "game5",
